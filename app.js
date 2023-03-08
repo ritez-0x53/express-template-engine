6080dc0fd790b39056f9671fb0908d971617819e
Ritez-Coder <riteswarsrgy@gmail.com>
2023-03-08 15:57:13 +0530

    adding comments app.js

--- app.js
+++ app.js
@@ -13,15 +13,21 @@ const pages = {
 //! view engine...
 app.set("view engine", "ejs");
 
+// root route
 app.get("/", (req, res) => {
+  // rendering ejs file and passing pages object in it
   res.render("./home.ejs", { pages: pages });
 });
 
+// profile route with get method.
 app.get("/profile", (req, res) => {
+  // rendering ejs file and passing pages object in it
   res.render("./profile.ejs", { pages });
 });
 
+// about route with get method
 app.get("/about", (req, res) => {
+  // rendering ejs file and passing pages object in it
   res.render("./about.ejs", { pages });
 });
 
